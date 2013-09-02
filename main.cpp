c31a7d9a8a641f7a845dd0532aee13d08f757452
Achal Dave <achalddave@live.com>
2013-09-02 01:28:49 +0100

    initial commit, saves a red image

---
+++ main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+#include <FreeImage.h>
+
+using std::cin;
+using std::cout; 
+using std::endl;
+
+void pause() {
+  int a;
+  cin >> a;
+}
+
+int main(int argc, char** argv) {
+  FIBITMAP *bitmap = FreeImage_Allocate(500, 500, 24);
+  RGBQUAD fi_color;
+  fi_color.rgbBlue = 255;
+  fi_color.rgbRed = 0;
+  fi_color.rgbGreen = 0;
+  for (int i = 0; i < 500; ++i) {
+    for (int j = 0; j < 500; ++j) {
+      FreeImage_SetPixelColor(bitmap, j, i, &fi_color);
+    }
+  }
+
+  if (FreeImage_Save(FIF_PNG, bitmap, "tmp.png", 0)) {
+    cout << "Image saved to " << "tmp.png" << endl;
+  } else {
+    cout << "Could not save the image to " << "tmp.png" << endl;
+  }
+  pause();
+}
